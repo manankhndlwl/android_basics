3192b12243334591db0d4d160a9d1be427e30de3
manankhndlwl <162955550+manankhndlwl@users.noreply.github.com>
2024-09-22 00:49:26 +0530

    greetings app

---
+++ app/src/main/java/com/example/greetingCard/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.greetingCard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.greetingCard.ui.theme.GreetingCardTheme
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GreetingCardTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Black, contentColor = Color.White) {
+        Column {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier.padding(24.dp)
+            )
+            Text(
+                text = "Hello $name!",
+                modifier = modifier.padding(1.dp)
+            )
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GreetingCardTheme {
+        Greeting("Manan")
+    }
+}
